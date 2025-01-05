2e4c502a4197afc46be2f1984aaf4d2414be972e
keianggoro12 <keianggoro12@gmail.com>
2025-01-05 14:00:01 +0700

    Initial commit for Playwright project

---
+++ tests/login.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+import { stepLogin } from "../pages/stepHook";
+
+const dataURL = require("../testData/dataURL.json");
+const dataLogin = require("../testData/dataLogin.json");
+
+test.describe("TS-1: Testing apa ini", () => {
+  let loginPage;
+  let context;
+  let page;
+
+  // Membuat context dan page sebelum semua tes
+  test.beforeAll(async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
+    loginPage = new stepLogin(page);
+
+    console.log("Opening URL...");
+    await loginPage.openUrl(dataURL.URL);
+    console.log("URL opened successfully.");
+  });
+
+  // TC-1: contoh case 1 bukar
+  test("TC-1: contoh case 1 buka", async () => {
+    // Testinggggg nya disini,,
+  });
+
+  // Menutup semua resource setelah semua tes selesai
+  test.afterAll(async () => {
+    try {
+      console.log("Closing all resources...");
+      if (page) {
+        await page.close();
+        console.log("Page closed.");
+      }
+      if (context) {
+        await context.close();
+        console.log("Context closed.");
+      }
+    } catch (error) {
+      console.error("Error during cleanup:", error);
+    }
+  });
+});
